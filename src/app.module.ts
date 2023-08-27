54591c90c8d4413ecb280b735510b906d0534ef2
Tirth <tirthacodes@gmail.com>
2023-08-27 21:15:50 +0545

    configured database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BlogsModule } from './blogs/blogs.module';
 
 @Module({
-  imports: [UsersModule, BlogsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'testuser',
+      password: 'testuser123',
+      database: 'nestjs_blog_platform',
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
+    }),
+    UsersModule, BlogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
