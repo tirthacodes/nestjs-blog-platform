8b6a385ba8438f44906198ed70363fdd9e208d40
Tirth <tirthacodes@gmail.com>
2023-08-30 09:19:22 +0545

    created endpoints in blog

--- src/blogs/controller/blogs/blogs.controller.ts
+++ src/blogs/controller/blogs/blogs.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Patch, Delete } from '@nestjs/common';
+import { BlogsService } from 'src/blogs/services/blogs/blogs.service';
 
 @Controller('blogs')
-export class BlogsController {}
+export class BlogsController {
+    
+    constructor(
+        private readonly blogService: BlogsService,
+    ){}
+
+    @Post('create')
+    createBlog(){
+
+    }
+
+    @Get()
+    getAllBlogs(){
+
+    }
+
+    @Get(':id')
+    getBlogById(){
+
+    }
+
+    @Patch('update/:id')
+    updateBlog(){
+
+    }
+
+    @Delete('delete/:id')
+    deleteBlog(){
+
+    }
+}
