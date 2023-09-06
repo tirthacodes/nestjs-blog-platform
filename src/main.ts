9a3412895949c22bedf3d7077ee8bd0d1d7a3f73
Tirth <tirthacodes@gmail.com>
2023-09-06 10:08:09 +0545

    validation works now!

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from "cookie-parser";
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.useGlobalPipes(new ValidationPipe);
+
   app.use(cookieParser());
   app.enableCors({
     origin: 'http://localhost:3000',
